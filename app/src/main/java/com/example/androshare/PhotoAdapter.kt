449c70766ad1ad7794e792dfd0d20af9272b0a25
Ola Awisat <ola.awi96@gmail.com>
2020-03-10 20:26:47 +0200

    Event page

--- app/src/main/java/com/example/androshare/PhotoAdapter.kt
+++ app/src/main/java/com/example/androshare/PhotoAdapter.kt
@@ -1,26 +1,45 @@
 package com.example.androshare
 
-import android.media.Image
+import android.content.Context
+import android.net.Uri
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
-class PhotoAdapter: BaseAdapter() {
 
+class PhotoAdapter(var context: Context, var images: List<Uri>) : BaseAdapter() {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        val imageView: ImageView
+        if (convertView == null) { // if it's not recycled, initialize some attributes
+            imageView = ImageView(context)
+            imageView.layoutParams = AbsListView.LayoutParams(100, 100)
+            imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+            imageView.setPadding(8, 8, 8, 8)
+            imageView.setOnClickListener {
+                // TODO implement on image click
+            }
+        } else {
+            imageView = convertView as ImageView
+        }
+        // TODO enable this (temporarily disabled to limit access to storage)
+//        Glide.with(context).load(images[position]).into(imageView)
+        return imageView
     }
 
     override fun getItem(position: Int): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return images[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return position.toLong()
     }
 
     override fun getCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return images.size
     }
 }
--- app/src/main/java/com/example/androshare/User.kt
+++ app/src/main/java/com/example/androshare/User.kt
@@ -1,14 +1,11 @@
 package com.example.androshare
 
 class User constructor(
-    givenName: String,
-    familyName: String,
-    email: String,
-    id: String
+    var givenName: String,
+    var familyName: String,
+    var email: String,
+    var id: String
 ) {
-    public var givenName : String? = givenName
-    public var familyName: String? = familyName
-    public var email: String = email
-    public var id: String = id
+    var avatar: Int = R.drawable.avatar1 // default avatar
 
 }
